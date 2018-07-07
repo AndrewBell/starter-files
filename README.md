624d232858e8f1048ce4f84e836f7e8581aa573b
Andrew Bell <andrew@recursivechaos.com>
2018-07-07 00:44:16 -0500

    updates bootstrap.sh to handle file conflicts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Starter Files
 
-A collection of common project files, and a utility to copy them.
+A collection of common project files and a utility to copy them to a new project.
 
 ## Goals
 
@@ -8,13 +8,22 @@ Easily bootstrap new projects with common files.
 
 ## Requirements
 
-BASH - Windows users can use [Git for windows](http://gitforwindows.org/)
+[Bash](https://www.gnu.org/software/bash/)
+    * [Windows via Git for Windows](http://gitforwindows.org/)
+    * OSX & (Most) Linux users should already have Bash
 
 [git](https://git-scm.com/downloads)
 
 ## Bootstrap a project
 
 `sh ./bootstrap.sh ../source-project`
+
+If conflicts are detected, the script will prompt you for action.
+
+## Todo
+
+* Language specific bootstrapping
+* Non-interactive flag for `bootstrap.sh`
 
 # Contact Me
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
 #
-# Copy starter files to destination directory
+# Copy starter files to destination directory.
+# `README_example.md` is copied instead of the project's README
+# If a file conflict is detected, script will prompt for action.
+#
+# Example: `sh ./bootstrap.sh ../source-project`
+
+declare -a FILES_TO_COPY=(
+  ".gitignore"
+  "Dockerfile"
+  "LICENSE.txt"
+  ".editorconfig"
+  "README.md")
 
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
 }
 
-if [ -z $1 ]; then
-  err "Please supply a relative source destination."
-  exit 1
-fi
+# Copy file to new directory, using their "example" if needed.
+bootstrapCopy() {
+  if [[ "${2}" = "README.md" ]]; then
+    cp ./README_example.md $2/README.md
+  else
+    cp ./$1 $2
+  fi
+  echo "$1 copied to $2/$1"
+}
 
-cp ./{.gitignore,Dockerfile,LICENSE.txt,.editorconfig} $1
+copyWithOvewritePrompt() {
+  local destination=$1
+  shift
+  local files=("$@")
 
-cp ./README_example.md $1/README.md
+  for file in "${files[@]}"; do
+      echo "$destination/$file"
+      if [ -f "$destination/$file" ]; then
+        echo "Would you like to overwrite $file? (y)es/(n)o/e(x)it"
+        read will_overwrite
+
+        if [[ "${will_overwrite}" = "y" ]]; then
+          bootstrapCopy $file $destination
+        elif [[ "${will_overwrite}" = "n" ]]; then
+          echo "Skipping $file"
+        elif [[ "${will_overwrite}" = "x" ]]; then
+          echo "kthxbai"
+          exit 0
+        else
+          echo "I don't know what to do with '${will_overwrite}', so I'm not going to overwrite $file"
+        fi
+      else
+        bootstrapCopy $file $destination
+      fi
+  done
+}
+
+validateArguments() {
+  if [ -z "$1" ]; then
+    err "Invalid Argument
+      Please supply a relative source destination.
+      Example: 'sh ./bootstrap.sh ../source-project'"
+    exit 1
+  fi
+}
+
+main() {
+    local source_directory=$1
+    validateArguments $source_directory
+    copyWithOvewritePrompt $source_directory "${FILES_TO_COPY[@]}"
+    echo "All done here."
+}
+
+main $@
